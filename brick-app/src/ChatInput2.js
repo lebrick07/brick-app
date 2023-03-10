92a3c79b189d4eaab979b5b0e44824bc4db9ec69
Idan Ben Yair <iyair@autometalabs.com>
2023-03-10 15:22:26 -0500

    Added request body params

--- brick-app/src/ChatInput2.js
+++ brick-app/src/ChatInput2.js
@@ -1,57 +1,3 @@
-// import React, { useState } from "react";
-
-// function ChatInput2() {
-//   const [message, setMessage] = useState("");
-//   const [response, setResponse] = useState("");
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-
-//     const url = "https://api.openai.com/v1/engines/davinci-codex/completions";
-//     const prompt = message;
-//     const maxTokens = 60;
-//     const apiKey = process.env.REACT_APP_CHATGPT_API_KEY; // Replace with your API key
-
-//     const response = await fetch(url, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//       body: JSON.stringify({
-//         prompt,
-//         max_tokens: maxTokens,
-//       }),
-//     });
-
-//     const data = await response.json();
-//     if (data.choices && data.choices.length > 0) {
-//       setResponse(data.choices[0].text);
-//     } else {
-//       setResponse("Sorry, no response was found.");
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Message:
-//           <input
-//             type="text"
-//             value={message}
-//             onChange={(e) => setMessage(e.target.value)}
-//           />
-//         </label>
-//         <button type="submit">Send</button>
-//       </form>
-//       <p>Response: {response}</p>
-//     </div>
-//   );
-// }
-
-// export default ChatInput2;
-
 import React, { useState } from "react";
 
 function ChatInput() {
@@ -63,8 +9,9 @@ function ChatInput() {
 
     const url = "https://api.openai.com/v1/chat/completions";
     const prompt = message;
-    const maxTokens = 60;
+    // const maxTokens = 60;
     const apiKey = process.env.REACT_APP_CHATGPT_API_KEY; // Replace with your API key
+    // const model = "davinci-codex";
 
     const response = await fetch(url, {
       method: "POST",
@@ -74,7 +21,9 @@ function ChatInput() {
       },
       body: JSON.stringify({
         prompt,
-        max_tokens: maxTokens,
+        temperature: 0.5,
+        max_tokens: 100,
+        model: "text-davinci-002"
       }),
     });
 
