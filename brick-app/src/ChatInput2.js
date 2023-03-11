8a26419e07f61767eab587872868233b4a54a376
Idan Ben Yair <iyair@autometalabs.com>
2023-03-11 17:03:32 -0500

    Working Chat

--- brick-app/src/ChatInput2.js
+++ brick-app/src/ChatInput2.js
@@ -1,61 +1,3 @@
-// import React, { useState } from "react";
-
-// function ChatInput() {
-//   const [message, setMessage] = useState("");
-//   const [response, setResponse] = useState("");
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-
-//     // const url = "https://api.openai.com/v1/chat/completions";
-//     const encodedURL = encodeURIComponent('https://api.openai.com/v1/chat/completions');
-//     const prompt = message;
-//     // const maxTokens = 60;
-//     const apiKey = process.env.REACT_APP_CHATGPT_API_KEY; // Replace with your API key
-//     // const model = "davinci-codex";
-
-//     const response = await fetch(encodedURL, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//       body: JSON.stringify({
-//         prompt,
-//         temperature: 0.5,
-//         max_tokens: 100,
-//         model: "text-davinci-002"
-//       }),
-//     });
-
-//     const data = await response.json();
-//     if (data.choices && data.choices.length > 0) {
-//       setResponse(data.choices[0].text);
-//     } else {
-//       setResponse("Sorry, no response was found.");
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Message:
-//           <input
-//             type="text"
-//             value={message}
-//             onChange={(e) => setMessage(e.target.value)}
-//           />
-//         </label>
-//         <button type="submit">Send</button>
-//       </form>
-//       <p>Response: {response}</p>
-//     </div>
-//   );
-// }
-
-// export default ChatInput;
-
 import React, { useState } from "react";
 
 function ChatInput2() {
@@ -66,34 +8,26 @@ function ChatInput2() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // const encodedURL = encodeURIComponent(
-    //     process.env.REACT_APP_OPENAI_URL
-    // );
     const prompt = message;
     const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
-    // const orgName = process.env.REACT_APP_OPENAI_ORG_NAME;
     const modelName = process.env.REACT_APP_OPENAI_MODEL;
-    // const apiUrl = `https://api.openai.com/organizations/${orgName}/models/${modelName}/completions`;
     const apiUrl = process.env.REACT_APP_OPENAI_URL;
-    // const apiUrl = encodedURL;
+
     const requestOptions = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${apiKey}`,
-        // Organization: org,
       },
       body: JSON.stringify({
         prompt,
         temperature: 0.5,
         max_tokens: 100,
         model: modelName,
-        // organization: orgName,
       }),
     };
 
     try {
-    //   const response = await fetch(`https://cors-anywhere.herokuapp.com/${apiUrl}`, requestOptions);
       const response = await fetch(apiUrl, requestOptions);
       const data = await response.json();
       if (data.choices && data.choices.length > 0) {
