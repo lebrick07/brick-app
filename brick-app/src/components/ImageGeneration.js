81e1e12af9c62c396b01be1997728775cf234b8b
Idan Ben Yair <iyair@autometalabs.com>
2023-03-13 19:42:02 -0400

    Image being created and displayed

--- brick-app/src/components/ImageGeneration.js
+++ brick-app/src/components/ImageGeneration.js
@@ -1,6 +1,62 @@
+// import React, { useState } from "react";
+// // import openai from "openai";
+// import axios from 'axios';
+
+// const apiEndpoint = "https://api.openai.com/v1/images/generations";
+
+// const ImageGeneration = () => {
+//   const [generatedImage, setGeneratedImage] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState("");
+
+//   const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+
+//   const generateImage = async () => {
+//     setLoading(true);
+//     setErrorMessage("");
+
+//     const conversation = "Dogs and cats";
+
+//     try {
+//       const response = await axios.post(apiEndpoint, {
+//         model: 'image-alpha-001',
+//         prompt: conversation,
+//         size: '1024x1024',
+//         n: 1,
+//       }, {
+//         headers: {
+//           'Content-Type': 'application/json',
+//           Authorization: `Bearer ${apiKey}`,
+//         },
+//         responseType: 'arraybuffer',
+//       });
+
+//       const base64Image = Buffer.from(response.data, 'binary').toString('base64');
+//       setGeneratedImage(`data:image/jpeg;base64,${base64Image}`);
+//     } catch (error) {
+//       console.error(error);
+//       setErrorMessage("Failed to generate image");
+//     }
+
+//     setLoading(false);
+//   };
+
+//   return (
+//     <div>
+//       {errorMessage && <div>{errorMessage}</div>}
+//       <button onClick={generateImage} disabled={loading}>
+//         {loading ? "Generating Image..." : "Generate Image"}
+//       </button>
+//       <br></br>
+//       {generatedImage && <img src={generatedImage} alt="Generated" crossorigin="anonymous"/>}
+//     </div>
+//   );
+// };
+
+// export default ImageGeneration;
+
 import React, { useState } from "react";
-// import openai from "openai";
-import axios from 'axios';
+import axios from "axios";
 
 const apiEndpoint = "https://api.openai.com/v1/images/generations";
 
@@ -18,21 +74,24 @@ const ImageGeneration = () => {
     const conversation = "Dogs and cats";
 
     try {
-      const response = await axios.post(apiEndpoint, {
-        model: 'image-alpha-001',
-        prompt: conversation,
-        size: '1024x1024',
-        n: 1,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${apiKey}`,
+      const response = await axios.post(
+        apiEndpoint,
+        {
+          model: "image-alpha-001",
+          prompt: conversation,
+          size: "1024x1024",
+          n: 1,
         },
-        responseType: 'arraybuffer',
-      });
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
 
-      const base64Image = Buffer.from(response.data, 'binary').toString('base64');
-      setGeneratedImage(`data:image/jpeg;base64,${base64Image}`);
+      const generatedImageUrl = response.data.data[0].url;
+      setGeneratedImage(generatedImageUrl);
     } catch (error) {
       console.error(error);
       setErrorMessage("Failed to generate image");
@@ -47,8 +106,8 @@ const ImageGeneration = () => {
       <button onClick={generateImage} disabled={loading}>
         {loading ? "Generating Image..." : "Generate Image"}
       </button>
-      <br></br>
-      {generatedImage && <img src={generatedImage} alt="Generated" crossorigin="anonymous"/>}
+      <br />
+      {generatedImage && <img src={generatedImage} alt="Generated" />}
     </div>
   );
 };
