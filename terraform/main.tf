0c584464dd2a2457f67790a8223f9e4544fea96f
Idan Ben Yair <iyair@autometalabs.com>
2023-03-16 00:39:44 -0400

    Making config changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,13 +32,6 @@ resource "aws_security_group" "allow_ssh" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port = 6443
-    to_port = 6443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   
   egress {
     from_port = 0
