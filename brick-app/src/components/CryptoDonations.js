e0bc33e9f894e47b7729db0c7be928d89c056d2b
Idan Ben Yair <iyair@autometalabs.com>
2023-03-21 01:07:59 -0400

    Removing old index.js

--- brick-app/src/components/CryptoDonations.js
+++ brick-app/src/components/CryptoDonations.js
@@ -1,23 +1,3 @@
-// import CryptoDonate from "dot-crypto-donate-react"
-// import React from "react";
-
-// <CryptoDonate
-//   cryptoDomain="lebrick07.eth"
-//   infuraApi="7dc42cded56a4d479e707e251e00d321"
-//   colors={{
-//     primary: "#2096f3",
-//     secondary: "#fde199",
-//     button: "#1a78c2",
-//     buttonSecondary: "#fab601",
-//     text: "#ffffff",
-//   }}
-//   text={{
-//     title: "Donate",
-//     thanks: "Thank you for donating, see the below link for transaction",
-//     copied: "Address copied, please donate via wallet",
-//   }}
-// />
-
 import React, { useState, useEffect } from 'react';
 import detectEthereumProvider from '@metamask/detect-provider';
 import Web3 from 'web3';
