064da425f337759cce23cb9fbf69623ebc4369c7
Idan Ben Yair <iyair@autometalabs.com>
2023-03-23 21:29:07 -0400

    Rolling back (disregard the AI Insights commit

--- brick-app/src/components/ChatInput.js
+++ brick-app/src/components/ChatInput.js
@@ -1,8 +1,4 @@
 import React, { useState, useRef } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
-// import { Prism } from "react-syntax-highlighter";
-
 
 function ChatInput() {
   const [message, setMessage] = useState("");
@@ -20,9 +16,7 @@ function ChatInput() {
     const modelName = process.env.REACT_APP_OPENAI_MODEL;
     const apiUrl = process.env.REACT_APP_OPENAI_URL;
 
-    const historyPrompt = chatHistory
-      .map((entry) => `User: ${entry.message}\n ${entry.response}`)
-      .join("\n\n");
+    const historyPrompt = chatHistory.map((entry) => `User: ${entry.message}\n ${entry.response}`).join("\n\n");
     const prompt = `${historyPrompt}\n\nUser: ${message}`;
     const requestOptions = {
       method: "POST",
@@ -45,18 +39,13 @@ function ChatInput() {
         const newResponse = data.choices[0].text;
         setResponse(newResponse);
         setError("");
-        setChatHistory((prevHistory) => [
-          ...prevHistory,
-          { message, response: newResponse },
-        ]);
+        setChatHistory((prevHistory) => [...prevHistory, { message, response: newResponse }]);
       } else {
         setResponse("");
         setError("Sorry, no response was found.");
       }
     } catch (error) {
-      setError(
-        "There was an error processing your request. Please try again later."
-      );
+      setError("There was an error processing your request. Please try again later.");
       setResponse("");
     }
 
@@ -65,35 +54,6 @@ function ChatInput() {
 
   const toggleChatHistory = () => {
     setShowChatHistory((prevShow) => !prevShow);
-  };
-  
-  const renderResponse = () => {
-    const codeSnippetRegex = /(?:```(?:\w+)?\s*)?((?:[^\n`]+\n?)+)(?:```)?/;
-    const match = codeSnippetRegex.exec(response);
-  
-    if (match) {
-      const codeSnippet = match[1];
-      const textBeforeCodeSnippet = response.slice(0, match.index).trim();
-      const textAfterCodeSnippet = response.slice(
-        match.index + match[0].length
-      ).trim();
-  
-      return (
-        <>
-          {textBeforeCodeSnippet && (
-            <p className="response-box2">{textBeforeCodeSnippet}</p>
-          )}
-          <SyntaxHighlighter style={dracula}>
-            {codeSnippet}
-          </SyntaxHighlighter>
-          {textAfterCodeSnippet && (
-            <p className="response-box2">{textAfterCodeSnippet}</p>
-          )}
-        </>
-      );
-    } else {
-      return <p className="response-box2">{response}</p>;
-    }
   };
 
   return (
@@ -109,7 +69,7 @@ function ChatInput() {
         <button type="submit">Send</button>
       </form>
       {error && <p>Error: {error}</p>}
-      {response && renderResponse()}
+      {response && <p className="response-box2">{response}</p>}
       <button onClick={toggleChatHistory}>
         {showChatHistory ? "Hide chat history" : "Show chat history"}
       </button>
@@ -124,6 +84,137 @@ function ChatInput() {
         </div>
       )}
     </div>
-  );  
-};
+  );
+}
+
 export default ChatInput;
+
+// import React, { useState, useRef } from "react";
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+// // import { Prism } from "react-syntax-highlighter";
+
+
+// function ChatInput() {
+//   const [message, setMessage] = useState("");
+//   const [response, setResponse] = useState("");
+//   const [error, setError] = useState("");
+//   const [chatHistory, setChatHistory] = useState([]);
+//   const [showChatHistory, setShowChatHistory] = useState(false);
+
+//   const chatHistoryRef = useRef(null);
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+
+//     const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+//     const modelName = process.env.REACT_APP_OPENAI_MODEL;
+//     const apiUrl = process.env.REACT_APP_OPENAI_URL;
+
+//     const historyPrompt = chatHistory
+//       .map((entry) => `User: ${entry.message}\n ${entry.response}`)
+//       .join("\n\n");
+//     const prompt = `${historyPrompt}\n\nUser: ${message}`;
+//     const requestOptions = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${apiKey}`,
+//       },
+//       body: JSON.stringify({
+//         prompt,
+//         temperature: 0.5,
+//         max_tokens: 2000,
+//         model: modelName,
+//       }),
+//     };
+
+//     try {
+//       const response = await fetch(apiUrl, requestOptions);
+//       const data = await response.json();
+//       if (data.choices && data.choices.length > 0) {
+//         const newResponse = data.choices[0].text;
+//         setResponse(newResponse);
+//         setError("");
+//         setChatHistory((prevHistory) => [
+//           ...prevHistory,
+//           { message, response: newResponse },
+//         ]);
+//       } else {
+//         setResponse("");
+//         setError("Sorry, no response was found.");
+//       }
+//     } catch (error) {
+//       setError(
+//         "There was an error processing your request. Please try again later."
+//       );
+//       setResponse("");
+//     }
+
+//     setMessage("");
+//   };
+
+//   const toggleChatHistory = () => {
+//     setShowChatHistory((prevShow) => !prevShow);
+//   };
+  
+//   const renderResponse = () => {
+//     const codeSnippetRegex = /(?:```(?:\w+)?\s*)?((?:[^\n`]+\n?)+)(?:```)?/;
+//     const match = codeSnippetRegex.exec(response);
+  
+//     if (match) {
+//       const codeSnippet = match[1];
+//       const textBeforeCodeSnippet = response.slice(0, match.index).trim();
+//       const textAfterCodeSnippet = response.slice(
+//         match.index + match[0].length
+//       ).trim();
+  
+//       return (
+//         <>
+//           {textBeforeCodeSnippet && (
+//             <p className="response-box2">{textBeforeCodeSnippet}</p>
+//           )}
+//           <SyntaxHighlighter style={dracula}>
+//             {codeSnippet}
+//           </SyntaxHighlighter>
+//           {textAfterCodeSnippet && (
+//             <p className="response-box2">{textAfterCodeSnippet}</p>
+//           )}
+//         </>
+//       );
+//     } else {
+//       return <p className="response-box2">{response}</p>;
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           <input
+//             type="text"
+//             value={message}
+//             onChange={(e) => setMessage(e.target.value)}
+//           />
+//         </label>
+//         <button type="submit">Send</button>
+//       </form>
+//       {error && <p>Error: {error}</p>}
+//       {response && renderResponse()}
+//       <button onClick={toggleChatHistory}>
+//         {showChatHistory ? "Hide chat history" : "Show chat history"}
+//       </button>
+//       {showChatHistory && (
+//         <div className="chat-history" ref={chatHistoryRef}>
+//           {chatHistory.map((entry, index) => (
+//             <div key={index}>
+//               <p>User: {entry.message}</p>
+//               <p>Bot: {entry.response}</p>
+//             </div>
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );  
+// };
+// export default ChatInput;
--- brick-app/src/components/pages/Contact.js
+++ brick-app/src/components/pages/Contact.js
@@ -1,61 +1,74 @@
 // src/components/pages/Contact.js
 
-import React, { useState } from 'react';
-import styles from '../../css/Contact.module.css';
+import React from 'react';
 
 const Contact = () => {
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    fetch('/submit-contact-form', {
-      method: 'POST',
-      body: formData,
-    })
-      .then((response) => {
-        if (response.ok) {
-          setMessage('Thank you for your message! We will be in touch soon.');
-        } else {
-          setMessage('Sorry, there was a problem sending your message. Please try again later.');
-        }
-      })
-      .catch((error) => {
-        setMessage('Sorry, there was a problem sending your message. Please try again later.');
-        console.error(error);
-      });
-  };
-
   return (
-    <div className={styles.contactContainer}>
+    <div>
       <h1>Contact</h1>
-      <p className={styles.contactDescription}>
-        Have a question, comment, or suggestion? Fill out the form below and
-        we'll get back to you as soon as possible!
-      </p>
-      <p>
-      You can also email us directly at: <a href="mailto:lebrick07@gmail.com">lebrick07@gmail.com</a>  
-      </p>
-      <form className={styles.contactForm} onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" required />
-        </div>
-        <button type="submit" className={styles.submitButton}>
-          Submit
-        </button>
-      </form>
-      {message && <p className={styles.message}>{message}</p>}
+      <p>This is the contact page.</p>
     </div>
   );
-};
+}
 
 export default Contact;
+
+// import React, { useState } from 'react';
+// import styles from '../../css/Contact.module.css';
+
+// const Contact = () => {
+//   const [message, setMessage] = useState('');
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+//     fetch('/submit-contact-form', {
+//       method: 'POST',
+//       body: formData,
+//     })
+//       .then((response) => {
+//         if (response.ok) {
+//           setMessage('Thank you for your message! We will be in touch soon.');
+//         } else {
+//           setMessage('Sorry, there was a problem sending your message. Please try again later.');
+//         }
+//       })
+//       .catch((error) => {
+//         setMessage('Sorry, there was a problem sending your message. Please try again later.');
+//         console.error(error);
+//       });
+//   };
+
+//   return (
+//     <div className={styles.contactContainer}>
+//       <h1>Contact</h1>
+//       <p className={styles.contactDescription}>
+//         Have a question, comment, or suggestion? Fill out the form below and
+//         we'll get back to you as soon as possible!
+//       </p>
+//       <p>
+//       You can also email us directly at: <a href="mailto:lebrick07@gmail.com">lebrick07@gmail.com</a>  
+//       </p>
+//       <form className={styles.contactForm} onSubmit={handleSubmit}>
+//         <div className={styles.formGroup}>
+//           <label htmlFor="name">Name:</label>
+//           <input type="text" id="name" name="name" required />
+//         </div>
+//         <div className={styles.formGroup}>
+//           <label htmlFor="email">Email:</label>
+//           <input type="email" id="email" name="email" required />
+//         </div>
+//         <div className={styles.formGroup}>
+//           <label htmlFor="message">Message:</label>
+//           <textarea id="message" name="message" required />
+//         </div>
+//         <button type="submit" className={styles.submitButton}>
+//           Submit
+//         </button>
+//       </form>
+//       {message && <p className={styles.message}>{message}</p>}
+//     </div>
+//   );
+// };
+
+// export default Contact;
