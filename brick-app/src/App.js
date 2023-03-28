bf662800e5d1d6822e6736abb1b6f928c8c145f0
Idan Ben Yair <iyair@autometalabs.com>
2023-03-27 21:16:18 -0400

    Corrections for model version

--- brick-app/src/App.js
+++ brick-app/src/App.js
@@ -1,18 +1,3 @@
-// // import logo from './logo.svg';
-// // import './css/App.css';
-// // import ChatInput from './ChatInput';
-// // import React from 'react';
-
-// // function App() {
-// //   return (
-// //     <div className="App">
-// //       <ChatInput />
-// //     </div>
-// //   );
-// // }
-
-// // export default App
-
 import React, { useState } from 'react';
 // import React from 'react';
 import ChatInput from './components/ChatInput';
