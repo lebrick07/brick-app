cddabb98b13f1b3a440ed4ffe3dc2a34c89a5cae
Skatz1990 <skatz1990@gmail.com>
2023-03-30 20:52:50 -0400

    Feature/dropdown (#6) (#7)
    * Added dropdown functionality
    
    * Added Audio support
    
    * Adding diagram
    
    * Adding diagram
    
    * Adding diagram
    
    * Adding diagram
    
    * Adding gitignore for React
    
    * Adding gitignore for React
    
    * Adding gitignore for React
    
    * added npm install instructions to readme
    
    ---------
    
    Co-authored-by: Idan Ben Yair <iyair@autometalabs.com>

--- brick-app/src/App.js
+++ brick-app/src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 // import React from 'react';
 import ChatInput from './components/ChatInput';
 import ImageGeneration from './components/ImageGeneration';
+import AudioTranscript from './components/AudioTranscript'
 import './css/App.css';
 import './css/ImageGeneration.css';
 import './css/Chat.css';
 import BrickBot from './images/brickbot.png';
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState('');
   const [conversationHistory, setConversationHistory] = useState([]);
   const [triggerImageGeneration, setTriggerImageGeneration] = useState(false);
 
@@ -15,27 +17,41 @@ function App() {
     setConversationHistory([...conversationHistory, newMessage]);
   };
 
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Brickbot</h1>
-      <img 
+      <img
         style={{ width: '200px', height: 'auto' }}
         src={BrickBot}
         alt="Brickbot">
       </img>
       <h2>Ask me anything..</h2>
-      <ChatInput
-        onNewMessage={handleNewMessage}
-        onTriggerImageGeneration={() => setTriggerImageGeneration(true)}
-      />
-      <br></br>
-      <br></br>
-      <h2>Wanna see a cool image?</h2>
-      <ImageGeneration
-        conversationHistory={conversationHistory}
-        triggerImageGeneration={triggerImageGeneration}
-        onResetTrigger={() => setTriggerImageGeneration(false)}
-      />
+      <select value={selectedOption} onChange={handleOptionChange}>
+        <option value="">Select an option</option>
+        <option value="chat">Chat</option>
+        <option value="image">Image</option>
+        <option value="audio">Audio</option>
+      </select>
+      {selectedOption === 'chat' && (
+        <ChatInput
+          onNewMessage={handleNewMessage}
+          onTriggerImageGeneration={() => setTriggerImageGeneration(true)}
+        />
+      )}
+      {selectedOption === 'image' && (
+        <ImageGeneration
+          conversationHistory={conversationHistory}
+          triggerImageGeneration={triggerImageGeneration}
+          onResetTrigger={() => setTriggerImageGeneration(false)}
+        />
+      )}
+      {selectedOption === 'audio' && (
+        <AudioTranscript/>
+      )}
     </div>
   );
 }
