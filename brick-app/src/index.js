fd223affb9ecf8573dc85183fe7e02fd0e59710d
Idan Ben Yair <iyair@autometalabs.com>
2023-04-03 15:48:49 -0400

    Fixing ReactDOM error

--- brick-app/src/index.js
+++ brick-app/src/index.js
@@ -189,8 +189,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MainApp />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
