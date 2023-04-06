0fb0f066b36f14cb7bd68fec255cf8d864bf9824
Idan Ben Yair <iyair@autometalabs.com>
2023-04-05 23:22:57 -0400

    Adding ALB redirect of listeners

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,6 @@ provider "aws" {
 
 resource "aws_security_group" "allow_ssh" {
   name_prefix = "allow_ssh"
-  
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port = 80
