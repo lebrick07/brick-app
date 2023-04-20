ecfed7508e1f8fe274edc0bbc65f58bc5d793b4a
skatz1990 <skatz1990@gmail.com>
2023-04-19 21:47:53 -0400

    Fixed expire completely

--- brick-app-api/routes/Sessions.js
+++ brick-app-api/routes/Sessions.js
@@ -3,30 +3,50 @@ function addSession(sessionId, expires, data, connection) {
         connection.query('SELECT session_id, expires, data FROM Sessions WHERE session_id = ?', [sessionId], function (err, results) {
             if (err) {
                 reject(err);
-            } else {
-                if (results.length === 0 ||
-                    (results.length > 0 && results[0].expires < Date.now())) {
-                    connection.query('INSERT INTO Sessions (session_id, expires, data) VALUES (?, ?, ?)',
-                        [sessionId, expires, data],
-                        function (err, results) {
-                            if (err) {
-                                reject(err);
-                            } else {
-                                resolve({
-                                    sessionId: results.session_id,
-                                    expires: expires,
-                                    data: data
-                                });
-                            }
-                        }
-                    );
-                } else {
-                    resolve(results[0]);
-                }
+                return;
             }
-        }
-        );
-    });
+
+            // Session found
+            if (results.length > 0) {
+                // Session not expired
+                if (results[0].expires >= Date.now()) {
+                    resolve(results[0]);
+                    return; ß
+                }
+
+                // Is it expired? Update its "expire" field
+                connection.query('UPDATE Sessions SET expires = ? WHERE session_id = ?', [expires, sessionId],
+                    function (err, results) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve({
+                                sessionId: results.session_id,
+                                expires: expires,
+                                data: data
+                            });
+                        }
+                    }
+                );
+                // Session not found. Create one
+            } else {
+                connection.query('INSERT INTO Sessions (session_id, expires, data) VALUES (?, ?, ?)',
+                    [sessionId, expires, data],
+                    function (err, results) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve({
+                                sessionId: results.session_id,
+                                expires: expires,
+                                data: data
+                            });
+                        }
+                    }
+                );
+            }
+        });
+    })
 }
 
 module.exports = { addSession };
